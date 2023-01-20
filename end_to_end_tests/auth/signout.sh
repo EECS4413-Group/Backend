823074266e496865578b555fba43e19e9296da36
Eli Frungorts <eli.frungorts@gmail.com>
2023-01-20 01:14:54 -0500

    moved extra files

--- end_to_end_tests/auth/signout.sh
+++
@@ -1,3 +0,0 @@
-curl -i -X POST http://localhost:8080/sign_out \
-  -H 'Content-Type: application/json' \
-  -H @token.txt
--- end_to_end_tests/auth/signup.sh
+++
@@ -1,3 +0,0 @@
-curl -i -X POST http://localhost:8080/sign_up \
-  -H 'Content-Type: application/json' \
-  -d @body.json
