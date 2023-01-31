74d2c4b827a7d044adf6da8a707995e89dca1e07
Eli Frungorts <eli.frungorts@gmail.com>
2023-01-31 11:54:47 -0500

    forgot to add file :)

---
+++ scripts/create_databases.sh
@@ -0,0 +1,5 @@
+echo "SELECT 'CREATE DATABASE auth' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auth')\gexec" | psql postgres -h localhost -p 5432 --username admin
+echo "SELECT 'CREATE DATABASE marketplace' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'marketplace')\gexec" | psql postgres -h localhost -p 5432 --username admin
+echo "SELECT 'CREATE DATABASE catalog' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'catalog')\gexec" | psql postgres -h localhost -p 5432 --username admin
+echo "SELECT 'CREATE DATABASE wallet' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'wallet')\gexec" | psql postgres -h localhost -p 5432 --username admin
+echo "SELECT 'CREATE DATABASE auction_daemon' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'auction_daemon')\gexec" | psql postgres -h localhost -p 5432 --username admin
